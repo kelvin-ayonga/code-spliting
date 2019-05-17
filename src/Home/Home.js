6fc6647a462f034eb82efc1799bbb6c988b570e2
kelvin-ayonga <kelvin.ayonga@sunergix.com>
2019-05-17 07:33:10 +0300

    Initial commit

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "./Home.css";
+
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return <div className="Home">Home</div>
+    }
+}
+
+export default Home;
